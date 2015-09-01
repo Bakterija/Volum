c150a83a1f48165ea74063fc8f47974ef3adf6ca
Bakterija <fixmix@inbox.lv>
2015-09-02 00:52:38 +0300

    Added a check to see if qpaeq is installed and translated the few remaining parts to english

--- Volum.py
+++ Volum.py
@@ -11,13 +11,13 @@ sink = find_sinks.read_settings('default_sink = ')
 print (sink_list)
 print ('Default sink: ', sink)
 pygame.init()
-gameDisplay = pygame.display.set_mode((500,300))
+Display = pygame.display.set_mode((500,300))
 pygame.display.set_caption('Volum')
-ikona = pygame.image.load('load/volum.png')
-eq_bilde = pygame.image.load('load/eqpic.png')
-eq_bilde_hov = pygame.image.load('load/eqpic_hov.png')
+icon = pygame.image.load('load/volum.png')
+eq_pic = pygame.image.load('load/eqpic.png')
+eq_pic_hov = pygame.image.load('load/eqpic_hov.png')
 clock = pygame.time.Clock()
-pygame.display.set_icon(ikona)
+pygame.display.set_icon(icon)
 
 white = (255,255,255)
 grey = (235,235,235)
@@ -27,10 +27,10 @@ dark = (35,35,35)
 blue = (80,80,150)
 green = (0,255,0)
 
-volum = 50
+volume = 50
 
-def eq_poga(x,y,bilde):
-    gameDisplay.blit(bilde,(x,y))
+def eq_button(x,y,picture):
+    Display.blit(picture,(x,y))
     
 def draw_text(text,variable,x,y,size,color,font):
     if font == 1:
@@ -39,31 +39,29 @@ def draw_text(text,variable,x,y,size,color,font):
         font_path = os.getcwd()+('/load/font2.ttf')
     font = pygame.font.Font(font_path,size)
     text = font.render((text + str(variable)), True, color)
-    gameDisplay.blit(text, (x,y))
+    Display.blit(text, (x,y))
 
-def draw_bar(bars_x, bars_y, bars_w, bars_h, color):  
-    pygame.draw.rect(gameDisplay, color, [bars_x, bars_y, bars_w, bars_h])
+def draw_bar(bar_x, bar_y, bar_w, bar_h, color):  
+    pygame.draw.rect(Display, color, [bar_x, bar_y, bar_w, bar_h])
 
-def skalak():
-    global volum
-    if volum < 150:
-        volum+=5
-    sys_arg_volum(volum)
+def higher():
+    global volume
+    volume+=5
+    sys_arg_volum(volume)
 
-def klusak():
-    global volum
-    if volum > 0:
-        volum-=5
-    sys_arg_volum(volum)
+def lower():
+    global volume
+    volume-=5
+    sys_arg_volum(volume)
 
-def sys_arg_volum(volum):
+def sys_arg_volum(volume):
     global sink
-    os.system("load/./arg_pactl.sh %s %s" % (sink, volum))
+    os.system("load/./arg_pactl.sh %s %s" % (sink, volume))
 
 def equalizer():
     os.system("load/./equalizer.sh")
 
-def get_volums():
+def get_volume():
     komanda = subprocess.Popen(["amixer"], stdout=subprocess.PIPE)
     output = komanda.communicate()[0]
     output2 = output.decode('utf-8')
@@ -74,13 +72,14 @@ def get_volums():
 
 
 def main_loop():
-    global volum
+    global volume
     global sink
     global sink_list
+    check_equalizer = os.path.exists('/usr/bin/qpaeq')
     sink_count = len(sink_list)
     redraw = True
     eq_hov = False
-    bilde = eq_bilde
+    eq_draw = eq_pic
     while True:
         for event in pygame.event.get():
             mouse_pos = pygame.mouse.get_pos()
@@ -90,17 +89,17 @@ def main_loop():
                 quit()
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_d and volum<150:
-                    skalak()
+                if event.key == pygame.K_d and volume<150:
+                    higher()
                     redraw = True
-                if event.key == pygame.K_a and volum>0:
-                    klusak()
+                if event.key == pygame.K_a and volume>0:
+                    lower()
                     redraw = True
-                if event.key == pygame.K_RIGHT and volum<150:
-                    skalak()
+                if event.key == pygame.K_RIGHT and volume<150:
+                    higher()
                     redraw = True     
-                if event.key == pygame.K_LEFT and volum>0:
-                    klusak()
+                if event.key == pygame.K_LEFT and volume>0:
+                    lower()
                     redraw = True
                 if event.key == pygame.K_1:
                     if sink_count > 0:
@@ -133,47 +132,49 @@ def main_loop():
                         find_sinks.write_settings('default_sink',sink)
                         redraw = True
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 4 and volum<150:
-                    skalak()
+                if event.button == 4 and volume<150:
+                    higher()
                     redraw = True
-                if event.button == 5 and volum>0:
-                    klusak()
+                if event.button == 5 and volume>0:
+                    lower()
                     redraw = True
                 elif event.button == 1:
-                    if mouse_pos[0] < 40+64 and mouse_pos[0] > 40:
-                        if mouse_pos[1] < 200+64 and mouse_pos[1] > 200:
-                            equalizer()
-                            redraw = True
+                    if check_equalizer == True:
+                        if mouse_pos[0] < 40+64 and mouse_pos[0] > 40:
+                            if mouse_pos[1] < 200+64 and mouse_pos[1] > 200:
+                                equalizer()
+                                redraw = True
 
         
         if mouse_pos[0] < 40+64 and mouse_pos[0] > 40:
             if mouse_pos[1] < 200+64 and mouse_pos[1] > 200:
-                bilde = eq_bilde_hov
+                eq_draw = eq_pic_hov
                 redraw = True
                 eq_hov = True
 
                 
         if eq_hov == True:
             if mouse_pos[0] > 40+48 or mouse_pos[0] < 40 or mouse_pos[1] > 200+48 or mouse_pos[1] < 200:
-                bilde = eq_bilde
+                eq_draw = eq_pic
                 redraw = True
                 eq_hov = False
                   
         
         if redraw == True:
-            gameDisplay.fill(grey)
+            Display.fill(grey)
 
             draw_bar(38,38,150*2.8+4,34,black)
-            if volum <105:
-                draw_bar(40,40,volum*2.8,30,red)
-            if volum > 100:
+            if volume <105:
+                draw_bar(40,40,volume*2.8,30,red)
+            if volume > 100:
                 draw_bar(40,40,100*2.8,30,red)
-                draw_bar(40+100*2.8,40,(volum-100)*2.8,30,blue)
-            draw_text('',volum,45,42,22,white,1)
+                draw_bar(40+100*2.8,40,(volume-100)*2.8,30,blue)
+            draw_text('',volume,45,42,22,white,1)
             draw_text('Sink: ',sink_list[int(sink)],42,82,18,black,2)
-
-            eq_poga(40,200,bilde)
-                
+            
+            if check_equalizer == True:
+                eq_button(40,200,eq_draw)
+                    
             pygame.display.update()
             redraw = False
             
