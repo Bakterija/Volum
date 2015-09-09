beb84dfe15fac0f8d133179f9c2793ebccf84a24
Bakterija <fixmix@inbox.lv>
2015-09-09 21:44:57 +0300

    equalizer added

---
+++ install qpaeq equalizer(optional).sh
@@ -0,0 +1,17 @@
+## Source http://www.webupd8.org/2013/03/install-pulseaudio-with-built-in-system.html]
+
+
+wget http://cgit.freedesktop.org/pulseaudio/pulseaudio/plain/src/utils/qpaeq -O /tmp/qpaeq
+sudo install /tmp/qpaeq /usr/local/bin/
+
+sudo apt-get install python-dbus python-qt4 python-qt4-dbus pulseaudio-utils
+
+pulseaudio -k
+
+pulseaudio &
+
+sleep 1
+
+pactl load-module module-equalizer-sink
+
+pactl load-module module-dbus-protocol
--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 chmod +x load/arg_pactl.sh
 chmod +x load/devices.sh
 chmod +x load/equalizer.sh
+chmod +x Volum.py
+chmod +x "install qpaeq equalizer(optional).sh"
+chmod +x "make desktop launcher.py"
 sudo apt-get install pavucontrol
 sudo apt-get install python
 sudo apt-get install python-pygame
