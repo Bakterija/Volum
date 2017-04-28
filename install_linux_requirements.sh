d6fadf24f0ef7c2bb4f82093c2b746080637b86e
Atis <atiskr@gmail.com>
2017-04-28 17:43:43 +0300

    Removed qpaeq equalizer install script

--- install_linux_requirements.sh
+++ install_linux_requirements.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-chmod +x sPAGUI.py
-chmod +x "install qpaeq equalizer(optional).sh"
-chmod +x "install pulseaudio-equalizer (optional).sh"
+chmod +x "install_pulseaudio-equalizer (optional).sh"
 chmod +x "make desktop launcher.py"
+sudo apt-get update
 sudo apt-get install python
 sudo apt-get install python-tk
 sudo apt-get install python-imaging-tk
 echo .
 echo .
 echo .
-echo .
 echo Done
-sleep 4
+sleep 3
